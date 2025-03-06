dc70f484512f206966c4b0e455f2258a6c5e49dd
lindo315 <114475416+lindo315@users.noreply.github.com>
2025-03-06 09:46:43 +0200

    Deploy project to main branch

--- vite.config.ts
+++ vite.config.ts
@@ -13,11 +13,12 @@ export default defineConfig({
     react(),
     runtimeErrorOverlay(),
     themePlugin(),
+
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
+            m.cartographer()
           ),
         ]
       : []),
@@ -30,7 +31,7 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: ".", // This is the default value
     emptyOutDir: true,
   },
 });
